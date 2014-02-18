234f7fa744fab649541cba2441d594790e04435d
Olga Casian <dae.eklen@gmail.com>
2014-02-18 21:45:29 +0200

    prepare one-to-one messaging and history

--- ajax/get_older_messages.php
+++ ajax/get_older_messages.php
@@ -10,39 +10,37 @@ if (isset($_POST['from']) && isset($_POST['to']) && isset($_POST['offset'])){
 	$to = $_POST['to'];
 	$offset = $_POST['offset'];
 	
-	$sql = "SELECT * FROM (SELECT * FROM ".TABLE_PREFIX."chat_messages C 
+	$sql = "SELECT * FROM (SELECT * FROM %schat_messages C 
 		WHERE 
-		(C.to='".$to."' OR C.to='".$from."') AND (C.from='".$from."' OR C.from='".$to."') 
-		ORDER BY timestamp DESC LIMIT ".$offset.",15) AS res ORDER BY timestamp ASC";
+		(C.to='%s' OR C.to='%s') AND (C.from='%s' OR C.from='%s') 
+		ORDER BY timestamp DESC LIMIT %d,15) AS res ORDER BY timestamp ASC";
 		
 	$html = '';
-	$result = mysql_query($sql, $db);
+	$result = queryDB($sql, array(TABLE_PREFIX, $to, $from, $from, $to, $offset));
 
-	while($row = mysql_fetch_assoc($result)){
-		$sql_from = "SELECT first_name, last_name, member_id FROM ".TABLE_PREFIX."chat_members C INNER JOIN ".TABLE_PREFIX."members M USING (member_id) 
-				WHERE C.jid='".$row[from]."'";
-		$result_from = mysql_query($sql_from, $db);
-		$row_from = mysql_fetch_assoc($result_from);
+	foreach ($result as $row) {
+		$sql_from = "SELECT * FROM %schat_members C INNER JOIN %smembers M USING (member_id) 
+				WHERE C.jid='%s'";
+		$row_from = queryDB($sql_from, array(TABLE_PREFIX, TABLE_PREFIX, $row['from']), true);
 		
-		$sql_pass = "SELECT * FROM ".TABLE_PREFIX."chat_members C INNER JOIN ".TABLE_PREFIX."members M USING (member_id) WHERE C.jid='".$row[from]."'";
-		$result_pass = mysql_query($sql_pass, $db);
-		$row_pass = mysql_fetch_assoc($result_pass);
+		$sql_pass = "SELECT * FROM %schat_members C INNER JOIN %smembers M USING (member_id) WHERE C.jid='%s'";
+		$row_pass = queryDB($sql_pass, array(TABLE_PREFIX, TABLE_PREFIX, $row['from']), true);
 					
-		$mcrypt = new Anti_Mcrypt($row_pass[password]);
-		$msg = $mcrypt->decrypt($row[msg]);
+		$mcrypt = new Anti_Mcrypt($row_pass['password']);
+		$msg = $mcrypt->decrypt($row['msg']);
 		
 		$html .= "<hr/><table><tr>" . 
          					"<td  class='conversations_picture'>" . 
-                            "<img class='picture' src='" .$_base_path. "get_profile_img.php?id=" .$row_from[member_id]. "' alt='userphoto'/>" . 
+                            "<img class='picture' src='" .$_base_path. "get_profile_img.php?id=" .$row_from['member_id']. "' alt='userphoto'/>" . 
                         	"</td>" .
                         	
                         	"<td  class='conversations_middle'>" . 
-                        	"<label class='conversations_name'><a href='profile.php?id=" .$row_from[member_id]. "'>" .$row_from[first_name] . ' ' . $row_from[last_name]. "</a></label>" . 
+                        	"<label class='conversations_name'><a href='profile.php?id=" .$row_from['member_id']. "'>" .$row_from['first_name'] . ' ' . $row_from['last_name']. "</a></label>" . 
                         	"<div class='conversations_msg'>"  .urldecode($msg).  
 							"</div>" . 
                         	"</td>" . 
                         	
-                        	"<td class='conversations_time'>" . $row[timestamp] . "</td>" . 
+                        	"<td class='conversations_time'>" . $row['timestamp'] . "</td>" . 
                  "</tr></table>";
 	}	
 	echo $html;
@@ -51,39 +49,37 @@ if (isset($_POST['from']) && isset($_POST['to']) && isset($_POST['offset'])){
 	$from = $_POST['from'];
 	$to = $_POST['to'];
 	
-	$sql = "SELECT * FROM (SELECT * FROM ".TABLE_PREFIX."chat_messages C 
+	$sql = "SELECT * FROM (SELECT * FROM %schat_messages C 
 		WHERE 
-		(C.to='".$to."' OR C.to='".$from."') AND (C.from='".$from."' OR C.from='".$to."') 
+		(C.to='%s' OR C.to='%s') AND (C.from='%s' OR C.from='%s') 
 		ORDER BY timestamp DESC LIMIT 10) AS res ORDER BY timestamp ASC";
 		
 	$html = '';
-	$result = mysql_query($sql, $db);
+	$result = queryDB($sql, array(TABLE_PREFIX, $to, $from, $from, $to));
 
-	while($row = mysql_fetch_assoc($result)){
-		$sql_from = "SELECT first_name, last_name, member_id FROM ".TABLE_PREFIX."chat_members C INNER JOIN ".TABLE_PREFIX."members M USING (member_id) 
-				WHERE C.jid='".$row[from]."'";
-		$result_from = mysql_query($sql_from, $db);
-		$row_from = mysql_fetch_assoc($result_from);
+	foreach ($result as $row) {
+		$sql_from = "SELECT * FROM %schat_members C INNER JOIN %smembers M USING (member_id) 
+				WHERE C.jid='%s'";
+		$row_from = queryDB($sql_from, array(TABLE_PREFIX, TABLE_PREFIX, $row['from']), true);
 		
-		$sql_pass = "SELECT * FROM ".TABLE_PREFIX."chat_members C INNER JOIN ".TABLE_PREFIX."members M USING (member_id) WHERE C.jid='".$row[from]."'";
-		$result_pass = mysql_query($sql_pass, $db);
-		$row_pass = mysql_fetch_assoc($result_pass);
+		$sql_pass = "SELECT * FROM %schat_members C INNER JOIN %smembers M USING (member_id) WHERE C.jid='%s'";
+		$row_pass = queryDB($sql_pass, array(TABLE_PREFIX, TABLE_PREFIX, $row['from']), true);
 					
-		$mcrypt = new Anti_Mcrypt($row_pass[password]);
-		$msg = $mcrypt->decrypt($row[msg]);
+		$mcrypt = new Anti_Mcrypt($row_pass['password']);
+		$msg = $mcrypt->decrypt($row['msg']);
 		
 		$html .= "<hr/><table><tr>" . 
          					"<td  class='conversations_picture'>" . 
-                            "<img class='picture' src='" .$_base_path. "get_profile_img.php?id=" .$row_from[member_id]. "' alt='userphoto'/>" . 
+                            "<img class='picture' src='" .$_base_path. "get_profile_img.php?id=" .$row_from['member_id']. "' alt='userphoto'/>" . 
                         	"</td>" .
                         	
                         	"<td  class='conversations_middle'>" . 
-                        	"<label class='conversations_name'><a href='profile.php?id=" .$row_from[member_id]. "'>" .$row_from[first_name] . ' ' . $row_from[last_name]. "</a></label>" . 
+                        	"<label class='conversations_name'><a href='profile.php?id=" .$row_from['member_id']. "'>" .$row_from['first_name'] . ' ' . $row_from['last_name']. "</a></label>" . 
                         	"<div class='conversations_msg'>"  .urldecode($msg).  
 							"</div>" . 
                         	"</td>" . 
                         	
-                        	"<td class='conversations_time'>" . $row[timestamp] . "</td>" . 
+                        	"<td class='conversations_time'>" . $row['timestamp'] . "</td>" . 
                  "</tr></table>";
 	}	
 	echo $html;
